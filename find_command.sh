79206e738393ce77998fbe9594dd6e297ee8a8f8
Leslie Emery <leslem@users.noreply.github.com>
2026-01-07 14:43:06 -0800

    Add devel dir find command

--- find_command.sh
+++ find_command.sh
@@ -9,3 +9,6 @@ find ~/devel -type f -name "*.R" -not -path "*/renv/*" -exec grep -nw reduce {} 
 
 # Find all files in the current dir that were modified in the last 16 days and save info to a file
 find . -type f -mtime -16 -ls > recent_files.txt
+
+# Search for a string in all R files within my devel dir
+find ~/devel -name "*.R" -not -path "*/renv/*" -not -path "*/doc/*" -exec grep -H "mysearchstring" {} \;
